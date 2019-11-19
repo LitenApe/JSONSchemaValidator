fedf205902968c3f615facc706798acac3761322
Son Thanh Vo <sonvo95@outlook.com>
2019-11-19 21:39:58 +0100

    feat: Add string validation

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -11,29 +11,89 @@ const nullObject = {'key': null};
 const emptyRecursiveObject = {'key': {'key': {}}}
 
 describe('JSON Schema Validator', () => {
-    it('blueprint set as "false" always returns false', () => {
-        expect(validator(emptyObject, false)).toBe(false);
-        expect(validator(stringObject, false)).toBe(false);
-        expect(validator(numberObject, false)).toBe(false);
-        expect(validator(objectObject, false)).toBe(false);
-        expect(validator(arrayObject, false)).toBe(false);
-        expect(validator(falseObject, false)).toBe(false);
-        expect(validator(trueObject, false)).toBe(false);
-        expect(validator(nullObject, false)).toBe(false);
+    describe('boolean as blueprint', () => {
+        it('blueprint set as "false" always returns false', () => {
+            expect(validator(emptyObject, false)).toBe(false);
+            expect(validator(stringObject, false)).toBe(false);
+            expect(validator(numberObject, false)).toBe(false);
+            expect(validator(objectObject, false)).toBe(false);
+            expect(validator(arrayObject, false)).toBe(false);
+            expect(validator(falseObject, false)).toBe(false);
+            expect(validator(trueObject, false)).toBe(false);
+            expect(validator(nullObject, false)).toBe(false);
+        });
+    
+        it('blueprint set as "true" always returns false', () => {
+            expect(validator(emptyObject, true)).toBe(true);
+            expect(validator(stringObject, true)).toBe(true);
+            expect(validator(numberObject, true)).toBe(true);
+            expect(validator(objectObject, true)).toBe(true);
+            expect(validator(arrayObject, true)).toBe(true);
+            expect(validator(falseObject, true)).toBe(true);
+            expect(validator(trueObject, true)).toBe(true);
+            expect(validator(nullObject, true)).toBe(true);
+        });
     });
 
-    it('blueprint set as "true" always returns false', () => {
-        expect(validator(emptyObject, true)).toBe(true);
-        expect(validator(stringObject, true)).toBe(true);
-        expect(validator(numberObject, true)).toBe(true);
-        expect(validator(objectObject, true)).toBe(true);
-        expect(validator(arrayObject, true)).toBe(true);
-        expect(validator(falseObject, true)).toBe(true);
-        expect(validator(trueObject, true)).toBe(true);
-        expect(validator(nullObject, true)).toBe(true);
-    });
+    describe('string validation', () => {
+        it('has type "string" defined', () => {
+            const blueprint = { definitions: { key: { type: 'string' } } };
+            expect(validator(stringObject, blueprint)).toBe(true);
+        });
 
-    it('empty nested object returns true', () => {
-        expect(validator(emptyRecursiveObject, emptyRecursiveObject)).toBe(true);
+        it('has type "number" defined', () => {
+            const blueprint = { definitions: { key: { type: 'number' } } };
+            expect(validator(stringObject, blueprint)).toBe(false);
+        });
+
+        it('has type "boolean" defined', () => {
+            const blueprint = { definitions: { key: { type: 'boolean' } } };
+            expect(validator(stringObject, blueprint)).toBe(false);
+        });
+
+        it('has type "array" defined', () => {
+            const blueprint = { definitions: { key: { type: 'boolean' } } };
+            expect(validator(stringObject, blueprint)).toBe(false);
+        });
+
+        it('has custom validator function defined', () => {
+            const mockTrue = jest.fn().mockReturnValue(true);
+            const mockFalse = jest.fn().mockReturnValue(false);
+
+            const blueprintTrue = { definitions: { key: { type: 'string', validator: mockTrue } } };
+            const blueprintFalse = { definitions: { key: { type: 'string', validator: mockFalse } } };
+
+            expect(mockTrue).toBeCalledTimes(0);
+            expect(validator(stringObject, blueprintTrue)).toBe(true);
+            expect(mockTrue).toBeCalledTimes(1);
+
+            expect(mockFalse).toBeCalledTimes(0);
+            expect(validator(stringObject, blueprintFalse)).toBe(false);
+            expect(mockFalse).toBeCalledTimes(1);
+        });
+
+        it('has "enum" defined', () => {
+            const blueprint = { definitions: { key: { type: 'string', enum: ['value'] } } };
+            expect(validator({'key': 'value'}, blueprint)).toBe(true);
+            expect(validator({'key': 'invalid'}, blueprint)).toBe(false);
+        });
+
+        it('has "const" defined', () => {
+            const blueprint = { definitions: { key: { type: 'string', const: 'value' } } };
+            expect(validator({'key': 'value'}, blueprint)).toBe(true);
+            expect(validator({'key': 'invalid'}, blueprint)).toBe(false);
+        });
+
+        it('has "minLength" defined', () => {
+            const blueprint = { definitions: { key: { type: 'string', minLength: 2 } } };
+            expect(validator({'key': 'value'}, blueprint)).toBe(true);
+            expect(validator({'key': 'i'}, blueprint)).toBe(false);
+        });
+
+        it('has "maxLength" defined', () => {
+            const blueprint = { definitions: { key: { type: 'string', maxLength: 5 } } };
+            expect(validator({'key': 'value'}, blueprint)).toBe(true);
+            expect(validator({'key': 'invalid'}, blueprint)).toBe(false);
+        });
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,82 @@ type ArrayType = string[] | number[] | object[] | boolean[];
 
 type SchemaType = {
     [key: string]: string | number | boolean | SchemaType | ArrayType | null;
+};
+
+type Validator = (value: string | number | ArrayType) => boolean;
+
+type Definition = {
+    type: string;
+    [key: string]: string | string[] | number | Validator;
 }
 
-function validateSubPart(schema: SchemaType, blueprint: SchemaType): boolean {
+type Definitions = {
+    [key: string]: Definition;
+};
+
+type BlueprintType = {
+    title?: string;
+    definitions: Definitions
+};
+
+function validateString(value: string, blueprint: Definition): boolean {
+    if (blueprint.type !== 'string') return false;
+
+    if (blueprint['validator']) {
+        const validator = blueprint['validator'] as Validator
+        if (!validator(value)) return false;
+    }
+
+    if (blueprint['enum']) {
+        const enums = blueprint['enum'] as string[];
+        if (!enums.includes(value)) return false;
+    }
+
+    if (blueprint['const']) {
+        const constant = blueprint['const'] as string;
+        if (value !== constant) return false;
+    }
+
+    if (blueprint['minLength']) {
+        const threshold = blueprint['minLength'] as number;
+        if (value.length < threshold) return false;
+    }
+
+    if (blueprint['maxLength']) {
+        const threshold = blueprint['maxLength'] as number;
+        if (value.length > threshold) return false;
+    }
+
+    // TODO: Regex
+
+    // TODO: Format
+
+    return true;
+}
+
+function validateSubPart(schema: SchemaType, blueprint: Definitions): boolean {
     const keys = Object.keys(schema);
 
     for (let i = 0; i < keys.length; i++) {
         const key = keys[i];
         if (key in blueprint) {
-            if (typeof schema[key] === 'object') {
-                validateSubPart(schema[key] as SchemaType, blueprint[key] as SchemaType);
+            if (typeof schema[key] === 'string') {
+                if (!validateString(schema[key] as string, blueprint[key])) return false;
             }
         }
     }
     return true;
 }
 
-function validate(schema: SchemaType, blueprint: boolean | SchemaType = {}): boolean {
-    if (typeof blueprint === 'boolean') {
-        return blueprint;
-    }
+function validate(
+    schema: SchemaType,
+    blueprint: boolean | BlueprintType = false
+    ): boolean {
+    if (typeof blueprint === 'boolean') return blueprint;
 
-    if (typeof schema !== 'object') {
-        return false;
-    }
+    if (typeof schema !== 'object') return false;
 
-    return validateSubPart(schema, blueprint);
+    return validateSubPart(schema, blueprint.definitions);
 }
 
 module.exports = validate;
